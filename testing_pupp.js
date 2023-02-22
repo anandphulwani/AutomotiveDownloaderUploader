6cda039904829cf3c52e19f266c3cc300edf90c6
Anand Phulwani <anand_phulwani@yahoo.com>
2023-02-22 18:29:57 +0530

    Converted parts into functions.

--- testing_pupp.js
+++ testing_pupp.js
@@ -13,38 +13,106 @@ import puppeteer from "puppeteer";
 
     const [page] = await browser.pages();   
     
-    await page.goto('https://signin.coxautoinc.com/logout?bridge_solution=HME');
-    await page.waitForFunction("window.location.href.startsWith('https://homenetauto.signin.coxautoinc.com/?solutionID=HME_prod&clientId=')");
-    await page.type('#username', "");
-    await page.type('#username', "dinesharora80@gmail.com");
-    await page.click('#signIn');
-    console.log("Clicked Signed In");
-    var text = '← dinesharora80@gmail.com';
-    await page.waitForFunction(text => document.querySelector('#returnLink').innerText.includes(text),{},text);
-    console.log("Found returnLink");
-    await page.type('#password', "");
-    await page.type('#password', "kunsh123");
-    console.log("Filled password");
-    text = 'Sign in';
-    await page.waitForFunction(text => document.querySelector('#signIn').innerText.includes(text),{},text);
-    console.log("Found Sign In button");
-    await page.click('#signIn');
-    console.log("Clicked on sign in button");
-    await page.waitForFunction("window.location.href.startsWith('https://www.homenetiol.com/dashboard')");
-    await page.waitForSelector('dt[.bb-userdatum__valu]')
-    text = 'dinesharora80@gmail.com';
-    await page.waitForFunction(text => document.querySelector('.bb-userdatum__value').innerText.includes(text),{},text);
-    await new Promise(r => setTimeout(r, 5000));
-    console.log("Found");
+    await gotoPageAndCheckIfCurrentURLStartsWith(page, "https://signin.coxautoinc.com/logout?bridge_solution=HME", "https://homenetauto.signin.coxautoinc.com/?solutionID=HME_prod&clientId=", true)
+    //await gotoPageAndCheckIfCurrentURLStartsWith(page, "https://homenetauto.signin.coxautoinc.com/?solutionID=HME_prod&clientId=efb6cb5b4f2a401a8225c9f2e8c6313c", undefined, true)
+    await fillInTextbox (page, "#username", "dinesharora80@gmail.com");
+    await clickOnButton (page, "#signIn", "Next");
+    await waitForElementContainsText(page, "#returnLink", "← dinesharora80@gmail.com")
+    await fillInTextbox (page, "#password", "kunsh123");
+    await clickOnButton (page, "#signIn", "Sign in");
+    await checkIfCurrentURLStartsWith(page, "https://www.homenetiol.com/dashboard")
+    await waitForElementContainsHTML(page, "dt.bb-userdatum__value", "dinesharora80@gmail.com", true)
+    await waitForSeconds(10, true);
     await browser.close();
-    //process.exit(0);
+    process.exit(0);
 })();
 
-/*
-<input type="text" id="" name="username" autocomplete="username" maxlength="100" class="username-textInput form-control" value=""></input>
-<button id="signIn" type="submit" class="btn btn-primary ">Next</button>
-<input class="password-textInput form-control" id="password" type="password" name="password" autocomplete="current-password"></input>
-<button id="signIn" type="submit" class="btn btn-primary ">Sign in</button>
+async function fillInTextbox (page, selector, textToFill, debug = false) {
+    debug ? console.log("Waiting for the "+selector+" to load: Executing.") : "";
+    await page.waitForSelector(selector);
+    debug ? console.log("Waiting for the "+selector+" to load: Found.") : "";
 
+    debug ? console.log("Filling the "+selector+" with empty value: Executing.") : "";
+    //await page.evaluate( () => document.querySelector(selector).value = "")
+    //await page.type(selector, "");
+    await page.focus(selector);
+    await page.keyboard.down('Control');await page.keyboard.press('A');await page.keyboard.up('Control');
+    await page.keyboard.press('Backspace');
+    debug ? console.log("Filling the "+selector+" with empty value: Done.") : "";
+   
+    debug ? console.log("Checking if "+selector+" is empty: Executing.") : "";
+    await page.waitForFunction( selector => document.querySelector(selector).value == "", {}, selector)
+    debug ? console.log("Checking if "+selector+" is empty: Done.") : "";
 
-*/
+    debug ? console.log("Waiting for 10 seconds.") : "";
+    debug ? await new Promise(r => setTimeout(r, 10000)) : "";
+    debug ? console.log("Waiting for 10 seconds done.") : "";
+
+    debug ? console.log("Filling the "+selector+" now: Executing.") : "";
+    await page.type(selector, textToFill);
+    debug ? console.log("Filling the "+selector+" now: Done.") : "";
+    debug ? console.log("Checking if "+selector+" value matches filled: Executing.") : "";
+    await page.waitForFunction(
+        (args) => document.querySelector(args[0]).value == args[1], 
+        {}, [selector, textToFill]);
+    debug ? console.log("Checking if "+selector+" value matches filled: Done.") : "";
+}
+
+async function clickOnButton (page, selector, buttonText, debug = false) {
+    debug ? console.log("Waiting for the "+selector+" to load: Executing.") : "";
+    await page.waitForSelector(selector);
+    debug ? console.log("Waiting for the "+selector+" to load: Found.") : "";
+
+    debug ? console.log("Check if the "+selector+" contains text: "+buttonText+": Executing.") : "";
+    await page.waitForFunction(
+        (args) => document.querySelector(args[0]).innerText.includes(args[1]),
+        {},[selector, buttonText]);
+    debug ? console.log("Check if the "+selector+" contains text: "+buttonText+": Found.") : "";
+
+    debug ? console.log("Clicking the "+selector+" button: Executing.") : "";
+    await page.click(selector);
+    debug ? console.log("Clicking the "+selector+" button: Done.") : "";
+}
+
+async function waitForElementContainsText(page, selector, elementText, debug = false) {
+    debug ? console.log("Waiting for the "+selector+" to load: Executing.") : "";
+    await page.waitForSelector(selector);
+    debug ? console.log("Waiting for the "+selector+" to load: Found.") : "";
+
+    debug ? console.log("Waiting for "+elementText+" ("+selector+") text to show up: Executing.") : "";
+    await page.waitForFunction(
+        (args) => document.querySelector(args[0]).innerText.includes(args[1]),
+        {},[selector, elementText]);
+    debug ? console.log("Waiting for "+elementText+" ("+selector+") text to show up: Found.") : "";
+}
+
+async function waitForElementContainsHTML(page, selector, elementHTML, debug = false) {
+    debug ? console.log("Waiting for the "+selector+" to load: Executing.") : "";
+    await page.waitForSelector(selector);
+    debug ? console.log("Waiting for the "+selector+" to load: Found.") : "";
+
+    debug ? console.log("Waiting for "+elementHTML+" ("+selector+") text to show up: Executing.") : "";
+    await page.waitForFunction(
+        (args) => document.querySelector(args[0]).innerHTML.includes(args[1]),
+        {},[selector, elementHTML]);
+    debug ? console.log("Waiting for "+elementHTML+" ("+selector+") text to show up: Found.") : "";
+}
+
+async function checkIfCurrentURLStartsWith(page, partialURL, debug = false) {
+    debug ? console.log("Waiting for the current URL to match to: "+partialURL+": Executing.") : "";
+    await page.waitForFunction("window.location.href.startsWith('"+partialURL+"')");
+    debug ? console.log("Waiting for the current URL to match to: "+partialURL+": Matched.") : "";
+}
+
+async function gotoPageAndCheckIfCurrentURLStartsWith(page, URL, partialURL = URL , debug = false) {
+    debug ? console.log("Navigating to the URL: "+URL+": Executing.") : "";
+    await page.goto(URL);
+    debug ? console.log("Navigating to the URL: "+URL+": Done.") : "";
+    await checkIfCurrentURLStartsWith(page, partialURL, debug);
+}
+
+async function waitForSeconds(seconds, debug = false) {
+    console.log("Waiting start for "+seconds+" seconds: Executing.");
+    await new Promise(r => setTimeout(r, ( seconds * 1000 )));
+    console.log("Waiting start for "+seconds+" seconds: Done.");
+}
