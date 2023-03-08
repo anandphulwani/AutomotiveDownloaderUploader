56838193962ab77cd79c30b4cf6564639f6199e3
Anand Phulwani <anand_phulwani@yahoo.com>
2023-03-09 03:10:33 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { getChromeBookmark } from 'chrome-bookmark-reader';
 
 /* eslint-disable import/extensions */
 import { config } from './configs/config.js';
+import { getCredentialsForUsername } from './functions/configsupportive.js';
 import { msleep, sleep, waitForSeconds } from './functions/sleep.js';
 import { printSectionSeperator } from './functions/others.js';
 import { checkTimezone, checkTimeWithNTP } from './functions/time.js';
@@ -35,6 +36,13 @@ if (config.environment === 'production') {
 if (validateDealerConfigurationExcelFile() === 'error') {
     process.exit(0);
 }
+
+// if (validateConfigFile() === 'error') {
+//     process.exit(0);
+// }
+// if (validateBookmarks() === 'error') {
+//     process.exit(0);
+// }
 // readDealerConfigurationFormatted();
 
 await killChrome({
@@ -60,7 +68,7 @@ const bookmarks = getChromeBookmark(bookmarkPath, bookmarkOptions);
         process.exit(1);
     }
     const [page] = await browser.pages();
-    // Create raw protocol session.
+    // Create raw protocol session. // TODO: Check why minimized makes program hanging.
     // const session = await page.target().createCDPSession();
     // const { windowId } = await session.send('Browser.getWindowForTarget');
     // await session.send('Browser.setWindowBounds', { windowId, bounds: { windowState: 'minimized' } });
@@ -75,36 +83,47 @@ const bookmarks = getChromeBookmark(bookmarkPath, bookmarkOptions);
             // eslint-disable-next-line no-restricted-syntax
             for (const usernameLevelBookmark of usernameLevelBookmarks) {
                 console.log(chalk.cyan(`Reading Bookmarks for the Username: ${chalk.cyan.bold(usernameLevelBookmark.name)}`));
+                const credentials = getCredentialsForUsername(usernameLevelBookmark.name);
+                // console.log(credentials.username);
+                // console.log(credentials.password);
+                if (credentials === undefined) {
+                    console.log(
+                        chalk.white.bgYellow.bold(
+                            `WARNING: Credentials for ${usernameLevelBookmark.name} not found in config file, Please declare in config.`
+                        )
+                    );
+                    // eslint-disable-next-line no-continue
+                    continue;
+                }
 
                 await gotoPageAndWaitTillCurrentURLStartsWith(
                     page,
                     'https://signin.coxautoinc.com/logout?bridge_solution=HME',
                     'https://homenetauto.signin.coxautoinc.com/?solutionID=HME_prod&clientId='
                 ); // use  (..., undefined, true) as params
-                const username = 'dinesharora80@gmail.com';
-                const password = 'kunsh123';
-                await fillInTextbox(page, '#username', username);
+                await fillInTextbox(page, '#username', credentials.username);
                 await clickOnButton(page, '#signIn', 'Next');
-                console.log('001');
-                await waitForElementContainsText(page, '#returnLink', `← ${username}`, true);
-                console.log('002');
-                await fillInTextbox(page, '#password', password);
+                // console.log('001');
+                await waitForElementContainsText(page, '#returnLink', `← ${credentials.username}`, true);
+                // console.log('002');
+                await fillInTextbox(page, '#password', credentials.password);
                 await clickOnButton(page, '#signIn', 'Sign in');
-                console.log('003');
+                // console.log('003');
                 await waitTillCurrentURLStartsWith(page, 'https://www.homenetiol.com/dashboard');
-                console.log('004');
+                // console.log('004');
                 await page.waitForSelector('#bridge-bar-user-menu');
-                console.log('005');
+                // console.log('005');
                 await page.waitForSelector('.bb-logout');
-                console.log('006');
+                // console.log('006');
 
+                // TODO: Enable the below block
                 // eslint-disable-next-line no-undef, no-loop-func
                 // await page.waitForFunction((args) => document.querySelector(args[0]).innerHTML.toLowerCase() === args[1].toLowerCase(), {}, [
                 //     'dt.bb-userdatum__value',
-                //     username,
+                //     credentials.username,
                 // ]);
-                console.log('007');
-                await waitForSeconds(5, true);
+                // console.log('007');
+                // await waitForSeconds(5, true);
 
                 const dealerLevelBookmarks = usernameLevelBookmark.children;
                 // dealerLevelBookmarks.forEach(async (dealerLevelBookmark) => {
