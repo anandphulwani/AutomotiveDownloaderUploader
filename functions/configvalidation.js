6ae352a4488cc414136582d7b08c79f0187e067d
Anand Phulwani <anand_phulwani@yahoo.com>
2023-03-15 00:37:33 +0530

    Added validateconfigfile functioality(incomplete)

---
+++ functions/configvalidation.js
@@ -0,0 +1,21 @@
+import chalk from 'chalk';
+import { getChromeBookmark } from 'chrome-bookmark-reader';
+
+/* eslint-disable import/extensions */
+import { config } from '../configs/config.js';
+import { getCredentialsForUsername } from './configsupportive.js';
+import { getAllDealerNames } from './excelsupportive.js';
+/* eslint-enable import/extensions */
+
+function validateConfigFile(debug = false) {
+    let validationStatus = 'success';
+    debug ? console.log(`Validating bookmarks and checking if credentials are present: Executing.`) : '';
+    validationStatus = 'success';
+    // TODO: Complete the validation of config file.
+    // validationStatus = 'error';
+    debug ? console.log(`Validating excel file: Done.`) : '';
+    return validationStatus;
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { validateConfigFile };
