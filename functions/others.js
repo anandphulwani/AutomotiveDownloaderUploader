36b8ffb7d02c0cd42328dd34b400de7df473f270
Anand Phulwani <anand_phulwani@yahoo.com>
2023-03-15 17:05:48 +0530

    Added retry variable and functions to others.js

--- functions/others.js
+++ functions/others.js
@@ -4,5 +4,15 @@ function printSectionSeperator() {
     console.log(chalk.black.bgWhiteBright('-'.repeat(80)));
 }
 
+// TODO: Remove retryCount variable and relative functions, when debugging is complete
+let retryCount = 0;
+function incRetryCount() {
+    retryCount++;
+}
+
+function getRetryCount() {
+    return retryCount;
+}
+
 // eslint-disable-next-line import/prefer-default-export
-export { printSectionSeperator };
+export { printSectionSeperator, incRetryCount, getRetryCount };
