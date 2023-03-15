65d9c88cda945e448344a97d65680a9d2da0c8dd
Anand Phulwani <anand_phulwani@yahoo.com>
2023-03-15 17:21:41 +0530

    Added retry functionality in goto functions if timeout occurs.

--- functions/goto.js
+++ functions/goto.js
@@ -1,10 +1,45 @@
-// eslint-disable-next-line import/extensions
+import chalk from 'chalk';
+import logSymbols from 'log-symbols';
+
+/* eslint-disable import/extensions */
+import { waitForSeconds } from './sleep.js';
+import { incRetryCount } from './others.js';
 import { waitForElementContainsText, waitForElementContainsHTML, waitTillCurrentURLStartsWith } from './waiting.js';
+/* eslint-enable import/extensions */
 
 async function gotoURL(page, URL, debug = false) {
     debug ? console.log(`Navigating to the URL: ${URL}: Executing.`) : '';
-    await page.goto(URL, { timeout: 180 * 1000 }); // waitUntil: 'load',
-    // await page.goto(URL, { waitUntil: "networkidle2" }); //TODO: Add networkidle0, networkidle2 and other multiple modes
+    for (let gotoCnt = 0; gotoCnt < 5; gotoCnt++) {
+        try {
+            await page.goto(URL, { timeout: 60 * 1000 }); // waitUntil: 'load',
+            // await page.goto(URL, { timeout: 10 }); // waitUntil: 'load',
+            // await page.goto(URL, { waitUntil: "networkidle2" }); //TODO: Add networkidle0, networkidle2 and other multiple modes
+            break;
+        } catch (err) {
+            if (err.message.match(/Navigation timeout of \d* ms exceeded/g) || err.message === 'socket hang up' || err.message === 'aborted') {
+                console.log(`SUCCESSFULLY ERROR HANDLED (WITHOUT HASH):#${err.message}#`);
+                process.stdout.write(chalk.yellow.bold(` ${logSymbols.warning}`));
+                if (gotoCnt < 4) {
+                    // Sleep for 30 seconds
+                    for (let cnt = 0; cnt < 10; cnt++) {
+                        process.stdout.write(chalk.yellow.bold('.'));
+                        await waitForSeconds(3);
+                    }
+                    incRetryCount();
+                } else {
+                    console.log(
+                        chalk.white.bgRed.bold(
+                            `\nUnable to get the following URL after 5 retries in interval of 30 seconds each, get operation timeout set to 60 seconds: ${URL} .`
+                        )
+                    );
+                    process.stdout.write('\t');
+                }
+            } else {
+                console.log(`CATCH THIS ERROR (WITHOUT HASH):#${err.message}#`);
+                throw err;
+            }
+        }
+    }
     debug ? console.log(`Navigating to the URL: ${URL}: Done.`) : '';
 }
 
