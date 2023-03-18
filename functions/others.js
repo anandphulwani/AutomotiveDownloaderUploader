f9167acdee1a9042021513e6d956c9d45a719077
Anand Phulwani <anand_phulwani@yahoo.com>
2023-03-19 03:07:27 +0530

    Added getSumOf2DArrayColumn and getIndexOfHighestIn2DArrayColumn functions

--- functions/others.js
+++ functions/others.js
@@ -14,5 +14,31 @@ function getRetryCount() {
     return retryCount;
 }
 
+function getSumOf2DArrayColumn(arr, columnIndex) {
+    let sumOfColumn = 0;
+    arr.forEach((sub) => {
+        sumOfColumn += sub[columnIndex];
+    });
+    return sumOfColumn;
+}
+
+function getIndexOfHighestIn2DArrayColumn(arr, columnIndex) {
+    let indexOfHighestNumber = null;
+    let highestNumber = null;
+    arr.forEach((sub, index) => {
+        if (indexOfHighestNumber === null && highestNumber === null) {
+            indexOfHighestNumber = index;
+            highestNumber = sub[columnIndex];
+            // console.log(`Inside1: ${indexOfHighestNumber}:${highestNumber}`);
+        } else if (sub[columnIndex] > highestNumber) {
+            indexOfHighestNumber = index;
+            highestNumber = sub[columnIndex];
+            // console.log(`Inside2: ${indexOfHighestNumber}:${highestNumber}`);
+        }
+        // console.log(`Outside: ${indexOfHighestNumber}:${highestNumber}`);
+    });
+    return indexOfHighestNumber;
+}
+
 // eslint-disable-next-line import/prefer-default-export
-export { printSectionSeperator, incRetryCount, getRetryCount };
+export { printSectionSeperator, getSumOf2DArrayColumn, getIndexOfHighestIn2DArrayColumn, incRetryCount, getRetryCount };
