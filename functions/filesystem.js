ca0aaa940b56396b67ee8b08a99358233a8cf844
Anand Phulwani <anand_phulwani@yahoo.com>
2023-03-21 15:40:47 +0530

    Modified and added few functions related to createDirAndMoveFile and removeDir

--- functions/filesystem.js
+++ functions/filesystem.js
@@ -49,32 +49,85 @@ async function createDirAndMoveFile(fromPath, toPath, debug = false) {
     await moveFile(fromPath, toPath, debug);
 }
 
-async function removeDir(dirPath, debug = false) {
-    await new Promise((resolve, reject) => {
-        fs.rm(
-            dirPath,
-            {
-                recursive: true,
-                maxRetries: 120,
-                retryDelay: 500,
-            },
-            (error) => {
-                if (error) {
-                    console.log(chalk.white.bgRed.bold(`Unable to remove a directory : ${dirPath}`));
-                    process.exit(1);
-                } else {
-                    debug ? console.log(`Folder path removed successfully : ${dirPath}`) : '';
-                    resolve();
-                }
-            }
-        );
-    });
-}
-
 async function createDirAndMoveFileFromTempDirToDestination(filePath, tempPath, destinationPath, debug = false) {
     debug ? console.log('Moving file from TempDir to Destination : Executing.') : '';
     await createDirAndMoveFile(filePath, filePath.replace(tempPath, destinationPath), debug);
     debug ? console.log('Moving file from TempDir to Destination : Done.') : '';
+}
+
+async function createDirAndMoveFileAndDeleteSourceParentFolderIfEmpty(fromPath, toPath, recursiveDeleteParentLevel = false, debug = false) {
+    await createDirAndMoveFile(fromPath, toPath, debug);
+    removeParentDirIfEmpty(fromPath, recursiveDeleteParentLevel, debug);
+}
+
+function removeDir(dirPath, recursiveDelete = false, debug = false) {
+    debug ? console.log('Removing Directory : Executing') : '';
+    // await new Promise((resolve, reject) => {
+    /* #region : Patch to delete a folder when recursiveDelete is false */
+    /**
+     * Note: Setting recursiveDelete to true because setting it to false is
+     * generating a `Path is a directory: ` error and fs.rmdir is deprecated.
+     * This is a nodejs problem.
+     */
+    if (!recursiveDelete) {
+        const dirPathCount = fs.readdirSync(dirPath).length;
+        if (dirPathCount > 0) {
+            console.log(chalk.white.bgRed.bold(`Unable to remove the directory, because it is not empty : ${dirPath}`));
+            process.exit(1);
+        }
+        recursiveDelete = true;
+    }
+    /* #endregion */
+    fs.rmSync(
+        dirPath,
+        {
+            recursive: recursiveDelete,
+            maxRetries: 120,
+            retryDelay: 500,
+        },
+        (error) => {
+            if (error) {
+                console.log(chalk.white.bgRed.bold(`Unable to remove a directory : ${dirPath}`));
+                process.exit(1);
+            } else {
+                debug ? console.log(`Folder path removed successfully : ${dirPath}`) : '';
+                // resolve();
+            }
+        }
+    );
+    // });
+    debug ? console.log('Removing Directory : Done') : '';
+}
+
+function removeParentDirIfEmpty(dirPath, recursiveDeleteParentLevel = false, debug = false) {
+    const recursiveParentLevel = recursiveDeleteParentLevel !== false ? recursiveDeleteParentLevel : 1;
+    // Run it so that it just not traverses above 3 directories
+    for (let loopIndex = 0; loopIndex < recursiveParentLevel; loopIndex++) {
+        const parentDir = path.dirname(dirPath);
+        const parentDirFilesCount = fs.readdirSync(parentDir).length;
+        debug
+            ? console.log(
+                  `parentDir: ${parentDir},    parentDirFilesCount: ${parentDirFilesCount} ${
+                      parentDirFilesCount === 0 ? ' (Removing)' : '(Breaking Here)'
+                  }`
+              )
+            : '';
+        if (parentDirFilesCount === 0) {
+            dirPath = parentDir;
+            console.log(`removeDir ${parentDir}`);
+            removeDir(parentDir, false, true);
+        } else {
+            console.log(`Directory (${parentDirFilesCount}) not empty: ${dirPath}`);
+            break;
+        }
+    }
+}
+
+function removeDirAndRemoveParentDirIfEmpty(dirPath, recursiveDeleteParentLevel = false, recursiveDelete = false, debug = false) {
+    debug ? console.log('Removing Directory And Removing Parent Directory If Empty : Executing') : '';
+    removeDir(dirPath, recursiveDelete, true);
+    removeParentDirIfEmpty(dirPath, recursiveDeleteParentLevel, true);
+    debug ? console.log('Removing Directory And Removing Parent Directory If Empty : Done') : '';
 }
 
 function generateTempFolderWithRandomText(debug = false) {
@@ -89,4 +142,12 @@ function generateTempFolderWithRandomText(debug = false) {
     return tempPathWithRandomFolder;
 }
 
-export { makeDir, moveFile, createDirAndMoveFile, removeDir, createDirAndMoveFileFromTempDirToDestination, generateTempFolderWithRandomText };
+export {
+    makeDir,
+    moveFile,
+    createDirAndMoveFileFromTempDirToDestination,
+    createDirAndMoveFileAndDeleteSourceParentFolderIfEmpty,
+    removeDir,
+    removeDirAndRemoveParentDirIfEmpty,
+    generateTempFolderWithRandomText,
+};
