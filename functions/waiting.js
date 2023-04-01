42749a89d5c1bb4aee63410055a5374e4bf34c0d
Anand Phulwani <anand_phulwani@yahoo.com>
2023-04-02 01:27:31 +0530

    Added option for exactMatch

--- functions/waiting.js
+++ functions/waiting.js
@@ -1,22 +1,38 @@
-async function waitForElementContainsText(page, selector, elementText, debug = false) {
+async function waitForElementContainsOrEqualsText(page, selector, elementText, exactMatch = false, debug = false) {
     debug ? console.log(`Waiting for the ${selector} to load: Executing.`) : '';
     await page.waitForSelector(selector, { timeout: 90000 });
     debug ? console.log(`Waiting for the ${selector} to load: Found.`) : '';
 
     debug ? console.log(`Waiting for ${elementText} (${selector}) text to show up: Executing.`) : '';
-    // eslint-disable-next-line no-undef
-    await page.waitForFunction((args) => document.querySelector(args[0]).innerText.includes(args[1]), { timeout: 90000 }, [selector, elementText]);
+    if (exactMatch) {
+        // eslint-disable-next-line no-undef
+        await page.waitForFunction((args) => document.querySelector(args[0]).innerText === args[1], { timeout: 90000 }, [selector, elementText]);
+    } else {
+        // eslint-disable-next-line no-undef
+        await page.waitForFunction((args) => document.querySelector(args[0]).innerText.includes(args[1]), { timeout: 90000 }, [
+            selector,
+            elementText,
+        ]);
+    }
     debug ? console.log(`Waiting for ${elementText} (${selector}) text to show up: Found.`) : '';
 }
 
-async function waitForElementContainsHTML(page, selector, elementHTML, debug = false) {
+async function waitForElementContainsOrEqualsHTML(page, selector, elementHTML, exactMatch = false, debug = false) {
     debug ? console.log(`Waiting for the ${selector} to load: Executing.`) : '';
     await page.waitForSelector(selector, { timeout: 90000 });
     debug ? console.log(`Waiting for the ${selector} to load: Found.`) : '';
 
     debug ? console.log(`Waiting for ${elementHTML} (${selector}) text to show up: Executing.`) : '';
-    // eslint-disable-next-line no-undef
-    await page.waitForFunction((args) => document.querySelector(args[0]).innerHTML.includes(args[1]), { timeout: 90000 }, [selector, elementHTML]);
+    if (exactMatch) {
+        // eslint-disable-next-line no-undef
+        await page.waitForFunction((args) => document.querySelector(args[0]).innerHTML === args[1], { timeout: 90000 }, [selector, elementHTML]);
+    } else {
+        // eslint-disable-next-line no-undef
+        await page.waitForFunction((args) => document.querySelector(args[0]).innerHTML.includes(args[1]), { timeout: 90000 }, [
+            selector,
+            elementHTML,
+        ]);
+    }
     debug ? console.log(`Waiting for ${elementHTML} (${selector}) text to show up: Found.`) : '';
 }
 
@@ -32,4 +48,4 @@ async function waitTillCurrentURLEndsWith(page, partialURL, debug = false) {
     debug ? console.log(`Waiting for the current URL to ends with: ${partialURL}: Matched.`) : '';
 }
 
-export { waitForElementContainsText, waitForElementContainsHTML, waitTillCurrentURLStartsWith, waitTillCurrentURLEndsWith };
+export { waitForElementContainsOrEqualsText, waitForElementContainsOrEqualsHTML, waitTillCurrentURLStartsWith, waitTillCurrentURLEndsWith };
