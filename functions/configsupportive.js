7d554897b7f90acc4212afae5d0ab4bb9d35bfcc
Anand Phulwani <anand_phulwani@yahoo.com>
2023-04-29 18:23:59 +0530

    Added getLastNonCatchErrorLogLevels9DigitUniqueId(), generateAndGetNonCatchErrorLogLevels9DigitUniqueId(), getLastCatchErrorLogLevels6DigitUniqueId(), generateAndGetCatchErrorLogLevels6DigitUniqueId() functions in configsupportive

--- functions/configsupportive.js
+++ functions/configsupportive.js
@@ -4,6 +4,7 @@ import { lockSync, unlockSync, checkSync } from 'proper-lockfile';
 
 /* eslint-disable import/extensions */
 import { config } from '../configs/config.js';
+import { zeroPad } from './stringformatting.js';
 import { waitForMilliSeconds } from './sleep.js';
 /* eslint-enable import/extensions */
 
@@ -117,26 +118,134 @@ async function setLastLotNumberAndDate(lastLotNumber, lastLotDate) {
                     configUser = newConfigUser;
                 }
 
-                if (currentLotLastRunDate !== lastLotDate) {
-                    const lastRunDateRegexString = `(    lotLastRunDate: ')(.*?)(',\\r\\n)`;
-                    const lastRunDateRegexExpression = new RegExp(lastRunDateRegexString, 'g');
-                    newConfigUser = configUser.replace(lastRunDateRegexExpression, `$1${lastLotDate}$3`);
-                    if (configUser === newConfigUser) {
-                        console.log(chalk.white.bgRed.bold(`Unable to set lastLotDate: '${lastLotDate}'. Serious issue, please contact developer.`));
-                        unlockSync('.\\configs\\config.js');
-                        process.exit(1);
-                    }
-                }
-                fs.writeFileSync('.\\configs\\config.js', newConfigUser, 'utf8');
-                unlockSync('.\\configs\\config.js');
-                break;
-            }
-        } catch (err) {
-            console.log(`${err.message}`);
+/* #region getLastNonCatchErrorLogLevels9DigitUniqueId(), generateAndGetNonCatchErrorLogLevels9DigitUniqueId() : Begin */
+function getLastNonCatchErrorLogLevels9DigitUniqueId() {
+    const configContent = fs.readFileSync('.\\configs\\config.js', 'utf8');
+    const lastNonCatchErrorRegexString = `(    nonCatchErrorLogLevels9DigitUniqueId: ')(.*?)(',\\r\\n)`;
+    const lastNonCatchErrorRegexExpression = new RegExp(lastNonCatchErrorRegexString, 'g');
+
+    const match = configContent.match(lastNonCatchErrorRegexExpression);
+    let lastNonCatchErrorLogLevels9DigitUniqueId = match[0].match(lastNonCatchErrorRegexString)[2];
+    lastNonCatchErrorLogLevels9DigitUniqueId = lastNonCatchErrorLogLevels9DigitUniqueId !== '' ? lastNonCatchErrorLogLevels9DigitUniqueId : '0';
+    return lastNonCatchErrorLogLevels9DigitUniqueId;
+}
+
+async function generateAndGetNonCatchErrorLogLevels9DigitUniqueId() {
+    const fileToOperateOn = '.\\configs\\config.js';
+    for (let lockTryIndex = 0; lockTryIndex <= 30; lockTryIndex++) {
+        if (lockTryIndex === 5) {
+            console.log(`Unable to get the lock`);
             process.exit(1);
         }
+        try {
+            const checkLock = checkSync(fileToOperateOn);
+            if (checkLock) {
+                await waitForMilliSeconds(50 + lockTryIndex * 3);
+                // eslint-disable-next-line no-continue
+                continue;
+            }
+            lockSync(fileToOperateOn);
+            break;
+        } catch (error) {
+            console.log(`${error.message}`);
+            console.log(`This piece of code should be unreachable.`);
+        }
     }
+
+    let nonCatchError;
+    try {
+        const currentNonCatchErrorLogLevels9DigitUniqueId = getLastNonCatchErrorLogLevels9DigitUniqueId();
+        const ConfigContent = fs.readFileSync(fileToOperateOn, 'utf8');
+
+        const currentNonCatchErrorRegexString = `(    nonCatchErrorLogLevels9DigitUniqueId: ')(.*?)(',\\r\\n)`;
+        const currentNonCatchErrorRegexExpression = new RegExp(currentNonCatchErrorRegexString, 'g');
+
+        nonCatchError = parseInt(currentNonCatchErrorLogLevels9DigitUniqueId, 10);
+        nonCatchError += 1;
+        nonCatchError = zeroPad(nonCatchError, 9);
+        const newConfigContent = ConfigContent.replace(currentNonCatchErrorRegexExpression, `$1${nonCatchError}$3`);
+
+        if (newConfigContent === undefined) {
+            console.log(
+                chalk.white.bgRed.bold(
+                    `Unable to set nonCatchErrorLogLevels9DigitUniqueId: '${nonCatchError}'. Serious issue, please contact developer.`
+                )
+            );
+            unlockSync(fileToOperateOn);
+            process.exit(1);
+        }
+        fs.writeFileSync(fileToOperateOn, newConfigContent, 'utf8');
+        unlockSync(fileToOperateOn);
+    } catch (err) {
+        console.log(`${err.message}`);
+        process.exit(1);
+    }
+    return nonCatchError;
 }
+/* #endregion getLastNonCatchErrorLogLevels9DigitUniqueId(), generateAndGetNonCatchErrorLogLevels9DigitUniqueId() : End */
+
+/* #region getLastCatchErrorLogLevels6DigitUniqueId(), generateAndGetCatchErrorLogLevels6DigitUniqueId() : Begin */
+function getLastCatchErrorLogLevels6DigitUniqueId() {
+    const configContent = fs.readFileSync('.\\configs\\config.js', 'utf8');
+    const lastCatchErrorRegexString = `(    catchErrorLogLevels6DigitUniqueId: ')(.*?)(',\\r\\n)`;
+    const lastCatchErrorRegexExpression = new RegExp(lastCatchErrorRegexString, 'g');
+
+    const match = configContent.match(lastCatchErrorRegexExpression);
+    let lastLastCatchErrorLogLevels6DigitUniqueId = match[0].match(lastCatchErrorRegexString)[2];
+    lastLastCatchErrorLogLevels6DigitUniqueId = lastLastCatchErrorLogLevels6DigitUniqueId !== '' ? lastLastCatchErrorLogLevels6DigitUniqueId : '0';
+    return lastLastCatchErrorLogLevels6DigitUniqueId;
+}
+
+async function generateAndGetCatchErrorLogLevels6DigitUniqueId() {
+    const fileToOperateOn = '.\\configs\\config.js';
+    for (let lockTryIndex = 0; lockTryIndex <= 30; lockTryIndex++) {
+        if (lockTryIndex === 5) {
+            process.exit(1);
+        }
+        try {
+            const checkLock = checkSync(fileToOperateOn);
+            if (checkLock) {
+                await waitForMilliSeconds(50 + lockTryIndex * 3, true);
+                // eslint-disable-next-line no-continue
+                continue;
+            }
+            lockSync(fileToOperateOn);
+            break;
+        } catch (error) {
+            console.log(`${error.message}`);
+            console.log(`This piece of code should be unreachable.`);
+        }
+    }
+
+    let catchError;
+    try {
+        const currentCatchErrorLogLevels6DigitUniqueId = getLastCatchErrorLogLevels6DigitUniqueId();
+        const configContent = fs.readFileSync(fileToOperateOn, 'utf8');
+
+        const currentCatchErrorRegexString = `(    catchErrorLogLevels6DigitUniqueId: ')(.*?)(',\\r\\n)`;
+        const currentCatchErrorRegexExpression = new RegExp(currentCatchErrorRegexString, 'g');
+
+        catchError = parseInt(currentCatchErrorLogLevels6DigitUniqueId, 10);
+        catchError += 1;
+        catchError = zeroPad(catchError, 6);
+        const newConfigContent = configContent.replace(currentCatchErrorRegexExpression, `$1${catchError}$3`);
+
+        if (newConfigContent === undefined) {
+            console.log(
+                chalk.white.bgRed.bold(`Unable to set catchErrorLogLevels6DigitUniqueId: '${catchError}'. Serious issue, please contact developer.`)
+            );
+            unlockSync(fileToOperateOn);
+            process.exit(1);
+        }
+        fs.writeFileSync(fileToOperateOn, newConfigContent, 'utf8');
+        unlockSync(fileToOperateOn);
+    } catch (err) {
+        console.log(`${err.message}`);
+        process.exit(1);
+    }
+    return catchError;
+}
+/* #endregion getLastCatchErrorLogLevels6DigitUniqueId(), generateAndGetCatchErrorLogLevels6DigitUniqueId() : End */
 
 // eslint-disable-next-line import/prefer-default-export
 export {
@@ -147,4 +256,6 @@ export {
     getContractorsCurrentAllotted,
     addToContractorsCurrentAllotted,
     setLastLotNumberAndDate,
+    generateAndGetNonCatchErrorLogLevels9DigitUniqueId,
+    generateAndGetCatchErrorLogLevels6DigitUniqueId,
 };
