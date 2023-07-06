5d58125ae85827fc07bc118a393a1e8e4857129f
Anand Phulwani <anand_phulwani@yahoo.com>
2023-07-06 12:46:31 +0530

    Created loggercyclicdependency.js to break cyclic dependency.

---
+++ functions/loggercyclicdependency.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/extensions */
+import { loggerFile, loggerConsole, addIndividualTransportCatcherrorFileWinston } from './logger.js';
+/* eslint-enable import/extensions */
+
+/**
+ *  Function is called by
+ *
+ * 01. loggeruniqueidgenerators.js
+ * 02. locksupportive.js
+ *
+ *  in order to break the chain of cyclic dependency.
+ *  The dependency is as follows
+ *
+ *  a. lgu, lgc, lge needs to generate unique ids.
+ *  b. generate unique id needs to take lock (attainlock,releaselock) on config.js to get id and increment it.
+ *  c. attainlock needs lgu,lgc,lge to log error if it occur.s
+ *
+ * @param  {...any} args
+ */
+const lgccyclicdependency = (...args) => {
+    addIndividualTransportCatcherrorFileWinston();
+    // const { filename, lineNumber } = getCallerDetails(...args);
+    const filename = 'loggercyclicdependency.js';
+    const lineNumber = '00';
+    const uniqueId = '######';
+    loggerConsole.catcherror(...args, { filename, lineNumber, uniqueId });
+    loggerFile.catcherror(...args, { filename, lineNumber, uniqueId });
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { lgccyclicdependency };
