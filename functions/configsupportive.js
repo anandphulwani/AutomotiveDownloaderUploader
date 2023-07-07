72401ceaaabfc51da13467e919985a61371a3bc6
Anand Phulwani <anand_phulwani@yahoo.com>
2023-07-07 18:45:05 +0530

    Removed non needed import.

--- functions/configsupportive.js
+++ functions/configsupportive.js
@@ -1,6 +1,5 @@
 import chalk from 'chalk';
 import fs from 'fs';
-import { lockSync, unlockSync, checkSync } from 'proper-lockfile';
 
 /* eslint-disable import/extensions */
 import { lgc } from './loggersupportive.js';
