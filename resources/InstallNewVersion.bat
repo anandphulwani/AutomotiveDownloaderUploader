43e5903bebf31cf751d294accb95e72dc6b77478
Anand Phulwani <anand_phulwani@yahoo.com>
2023-07-08 15:35:46 +0530

    Update InstallNewVersion batch and exe.

--- resources/InstallNewVersion.bat
+++ resources/InstallNewVersion.bat
@@ -18,34 +18,37 @@ REM BFCPEOPTIONEND
 @ECHO ON
 @echo off
 set /p version="Enter the version number: "
-set folder=AutomotiveDownloaderUploader%version%
+set "versionWith3Digits=00%version%"
+set "versionWith3Digits=%versionWith3Digits:~-3%"
+set folder=AutomotiveDownloaderUploader%versionWith3Digits%
 
 if exist %folder% (
     echo Folder %folder% already exists. Exiting...
     exit /b
-) else (
-    mkdir %folder%
-    cd %folder%
-    git clone https://github.com/anandphulwani/AutomotiveDownloaderUploader .
-    npm i
-    set /p runCount="Enter the number of runs (1-100, default is 1): "
-    set /a runCount=%runCount% 2>nul
-    if "%runCount%"=="" set /a runCount=1
-    if %runCount% lss 1 set /a runCount=1
-    if %runCount% gtr 100 set /a runCount=100
-    setlocal enabledelayedexpansion
-    for %%F in (RunDownloader.exe RunUploader.exe) do (
-        set "filename=%%~nF"
-        set "extension=%%~xF"
-        set "newname=%version%!filename!!extension!"
-        echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-        echo sLinkFile = "%USERPROFILE%\Desktop\!newname!.lnk" >> CreateShortcut.vbs
-        echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-        echo oLink.TargetPath = "%%F" >> CreateShortcut.vbs
-        echo oLink.Arguments = "!runCount!" >> CreateShortcut.vbs
-        echo oLink.Save >> CreateShortcut.vbs
-        cscript CreateShortcut.vbs
-        del CreateShortcut.vbs
-    )
-    endlocal
 )
+mkdir %folder%
+cd %folder%
+git clone https://github.com/anandphulwani/AutomotiveDownloaderUploader .
+rmdir /s /q resources
+call npm i -y || echo npm i resulted in non zero status, continuing...
+set /p runCount="Enter the number of runs (1-100, default is 1): "
+if not defined runCount set /a runCount=1
+set /a runCount=%runCount% 2>nul
+if %runCount% lss 1 set /a runCount=1
+if %runCount% gtr 100 set /a runCount=100
+setlocal enabledelayedexpansion
+for %%F in (RunDownloader.exe RunUploader.exe) do (
+    set "filename=%%~nF"
+    set "extension=%%~xF"
+    set "newname=%version%!filename!!extension!"
+    echo Set oWS = WScript.CreateObject^("WScript.Shell"^) > CreateShortcut.vbs
+    echo sLinkFile = "%USERPROFILE%\Desktop\!newname!.lnk" >> CreateShortcut.vbs
+    echo Set oLink = oWS.CreateShortcut^(sLinkFile^) >> CreateShortcut.vbs
+    echo oLink.TargetPath = "%CD%\%%F" >> CreateShortcut.vbs
+    echo oLink.Arguments = "!runCount!" >> CreateShortcut.vbs
+    echo oLink.Save >> CreateShortcut.vbs
+    cscript CreateShortcut.vbs
+    del CreateShortcut.vbs
+)
+endlocal
+cd ..
