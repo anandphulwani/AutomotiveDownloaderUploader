99c62a0e33010086be37f76bf6bb590c645231bf
Anand Phulwani <anand_phulwani@yahoo.com>
2023-07-09 22:10:07 +0530

    Removed pause statements added earlier for debugging

--- resources/InstallNewVersionReplacer.bat
+++ resources/InstallNewVersionReplacer.bat
@@ -1,6 +1,22 @@
-@echo on
-
-pause
+@ECHO OFF
+REM BFCPEOPTIONSTART
+REM Advanced BAT to EXE Converter www.BatToExeConverter.com
+REM BFCPEEXE=C:\Users\Administrator\Desktop\AutomotiveDownloaderUploader\resources\InstallNewVersionReplacer.exe
+REM BFCPEICON=C:\Program Files (x86)\Advanced BAT to EXE Converter PRO v2.91\ab2econv291pro\icons\icon11.ico
+REM BFCPEICONINDEX=1
+REM BFCPEEMBEDDISPLAY=0
+REM BFCPEEMBEDDELETE=1
+REM BFCPEADMINEXE=1
+REM BFCPEINVISEXE=0
+REM BFCPEVERINCLUDE=0
+REM BFCPEVERVERSION=1.0.0.0
+REM BFCPEVERPRODUCT=Product Name
+REM BFCPEVERDESC=Product Description
+REM BFCPEVERCOMPANY=Your Company
+REM BFCPEVERCOPYRIGHT=Copyright Info
+REM BFCPEOPTIONEND
+@ECHO ON
+@echo off
 
 :loop
 REM Check if InstallNewVersion.exe is running
@@ -11,9 +27,6 @@ IF "%ERRORLEVEL%"=="0" (
     GOTO loop
 )
 
-pause
-
 REM InstallNewVersion.exe is not running, so we can replace it
 echo InstallNewVersion.exe is not running, replacing it...
-copy /Y "%TEMP%\InstallNewVersion.exe" ".\InstallNewVersion.exe" 
-pause
+copy /Y "%TEMP%\InstallNewVersion.exe" ".\InstallNewVersion.exe"
