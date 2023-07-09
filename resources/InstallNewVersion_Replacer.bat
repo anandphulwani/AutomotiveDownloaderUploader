c673133fab2aec6050b370013c36ca0f92dbdbaf
Anand Phulwani <anand_phulwani@yahoo.com>
2023-07-09 21:19:50 +0530

    Added more pause for debugging

--- resources/InstallNewVersion_Replacer.bat
+++ resources/InstallNewVersion_Replacer.bat
@@ -1,5 +1,7 @@
 @echo on
 
+pause
+
 :loop
 REM Check if InstallNewVersion.exe is running
 tasklist /FI "IMAGENAME eq InstallNewVersion.exe" 2>NUL | find /I /N "InstallNewVersion.exe">NUL
@@ -8,6 +10,8 @@ IF "%ERRORLEVEL%"=="0" (
     timeout /t 5 /nobreak > NUL
     GOTO loop
 )
+
+pause
 
 REM InstallNewVersion.exe is not running, so we can replace it
 echo InstallNewVersion.exe is not running, replacing it...
