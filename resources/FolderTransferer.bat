0f8a9361ae55a432a540278cdf1285d744199d8b
Anand Phulwani <anand_phulwani@yahoo.com>
2023-10-20 23:50:06 +0530

    Shifted FolderTransferer in a separate exe.

---
+++ resources/FolderTransferer.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+REM BFCPEOPTIONSTART
+REM Advanced BAT to EXE Converter www.BatToExeConverter.com
+REM BFCPEEXE=.\FolderTransferer.exe
+REM BFCPEICON=.\ExecutableGear.ico
+REM BFCPEICONINDEX=-1
+REM BFCPEEMBEDDISPLAY=0
+REM BFCPEEMBEDDELETE=1
+REM BFCPEADMINEXE=1
+REM BFCPEINVISEXE=0
+REM BFCPEVERINCLUDE=0
+REM BFCPEVERVERSION=1.0.0.0
+REM BFCPEVERPRODUCT=Product Name
+REM BFCPEVERDESC=Product Description
+REM BFCPEVERCOMPANY=Your Company
+REM BFCPEVERCOPYRIGHT=Copyright Info
+REM BFCPEOPTIONEND
+@ECHO ON
+@echo off
+cd "%~dp0"
+cls
+for /L %%i in (1,1,15) do (
+    node contractors_folderTransferer.js
+    pause
+)
