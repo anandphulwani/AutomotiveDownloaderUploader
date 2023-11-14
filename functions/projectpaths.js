e15e6e90bbcbe3a31ea7d744f3f11a9c79c36b02
Anand Phulwani <anand_phulwani@yahoo.com>
2023-11-14 16:45:19 +0530

    Added `projectpaths.js` functionality.

---
+++ functions/projectpaths.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+function getProjectRootDirPath() {
+    // eslint-disable-next-line no-underscore-dangle
+    return path.dirname(path.dirname(fileURLToPath(import.meta.url)));
+}
+
+function getProjectConfigDirPath() {
+    return path.join(getProjectRootDirPath, 'configs');
+}
+
+function getProjectLogsDirPath() {
+    return path.join(getProjectRootDirPath, 'logs');
+}
+
+function getProjectConfigFilePath() {
+    return path.join(getProjectConfigDirPath, 'configs.js');
+}
+
+export { getProjectRootDirPath, getProjectConfigDirPath, getProjectLogsDirPath, getProjectConfigFilePath };
