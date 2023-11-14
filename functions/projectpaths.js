43d0e10cc666ad3f542c08b83e1dd629c0b4e76d
Anand Phulwani <anand_phulwani@yahoo.com>
2023-11-14 16:59:54 +0530

    Resolved bugs in `projectpaths.js`

--- functions/projectpaths.js
+++ functions/projectpaths.js
@@ -7,15 +7,15 @@ function getProjectRootDirPath() {
 }
 
 function getProjectConfigDirPath() {
-    return path.join(getProjectRootDirPath, 'configs');
+    return path.join(getProjectRootDirPath(), 'configs');
 }
 
 function getProjectLogsDirPath() {
-    return path.join(getProjectRootDirPath, 'logs');
+    return path.join(getProjectRootDirPath(), 'logs');
 }
 
 function getProjectConfigFilePath() {
-    return path.join(getProjectConfigDirPath, 'configs.js');
+    return path.join(getProjectConfigDirPath(), 'config.js');
 }
 
 export { getProjectRootDirPath, getProjectConfigDirPath, getProjectLogsDirPath, getProjectConfigFilePath };
