8b15c142da57de600807ca729760763b21664f0b
Anand Phulwani <anand_phulwani@yahoo.com>
2023-11-22 00:12:34 +0530

    Made major modifications to remove redundant code between `logFormatFile` and `logFormatConsole`.

--- functions/loggerLogFormats.js
+++ functions/loggerLogFormats.js
@@ -10,66 +10,108 @@ import LoggingPrefix from '../class/LoggingPrefix.js';
 
 const { printf } = format;
 
-// Define log functions
-/* #region logFormatFile and logFormatConsole : Begin */
-const lastWriteLineSepObj = {};
-const logFormatFile = (logFilename) =>
-    printf(({ level, message, timestamp: ts, stack, [Symbol.for('splat')]: sp }) => {
-        // console.log(`logFormatFile Called, level:${level}`);
-        const lastWriteLineSep = Object.prototype.hasOwnProperty.call(lastWriteLineSepObj, logFilename) ? lastWriteLineSepObj[logFilename] : true;
-        const { callerHierarchy, uniqueId, loggingPrefix, lineSep } =
-            sp !== undefined
-                ? sp.slice(-1)[0]
-                : { callerHierarchy: '', uniqueId: '', loggingPrefix: LoggingPrefix.true, lineSep: LineSeparator.true };
-        let logMesg = [];
-        if (loggingPrefix.name === true && lastWriteLineSep) {
-            ts !== undefined ? logMesg.push(ts) : null;
-            uniqueId !== undefined ? logMesg.push(`[${uniqueId.padStart(9, ' ')}]`) : null;
-            logMesg.push(`[${padStartAndEnd(`${level.toUpperCase() === 'WARN' ? 'WARNING' : level.toUpperCase()}`, 21, ' ')}]`);
-        }
-        // If custom message is sent then, the custom message is merged with the first line of error message.
-        if (stack !== undefined && stack.length > 0) {
-            if (message.includes(stack)) {
-                message = message.replace(stack, '');
-                message = message.trim();
-            }
-            stack = stack.split('\n');
-            const regex = new RegExp(`^(\\S+?):(?:[ ]?)${message}$`);
-            if (regex.test(stack[0])) {
-                stack.shift();
-                message = message.trim();
-            } else {
-                const errorString = stack[0].replace(/^[a-zA-Z]*Error:/, '').trim();
-                message = message.replace(errorString, '').trim();
-            }
-            stack = stack.join('\n');
-        }
-        logMesg.push(message);
-        if (callerHierarchy !== '' && sp !== undefined && lineSep.name === true) {
-            logMesg.push(`(${callerHierarchy})`);
-        }
-        logMesg = logMesg.join(' ');
-        if (stack !== undefined && !logMesg.includes(stack)) {
-            logMesg = `${logMesg}\n${stack}`;
-        }
-        if (lineSep.name) {
-            logMesg = `${logMesg}\n`;
-        }
-        lastWriteLineSepObj[logFilename] = lineSep.name;
-        return logMesg;
-    });
+// TODO: create stack color similar to the main color as for catcherror, if a custom color is given, as in the situation above, create similar to the custom color
+const stringToChalkColor = {
+    cyan: [chalk.cyan, chalk.cyan],
+    bgCyan: [chalk.bgCyan.bold, chalk.bgCyan.bold],
+    cyanBold: [chalk.cyan.bold, chalk.cyan.bold],
+    green: [chalk.green.bold, chalk.green.bold],
+    bgGreen: [chalk.white.bgGreen.bold, chalk.white.bgGreen.bold],
+    yellow: [chalk.yellow.bold, chalk.yellow.bold],
+    bgYellow: [chalk.white.bgYellow.bold, chalk.white.bgYellow.bold],
+    red: [chalk.red.bold, chalk.red.bold],
+    bgRed: [chalk.white.bgRed.bold, chalk.white.bgRed.bold],
+    magenta: [chalk.magenta.bold, chalk.magenta.bold],
+    bgMagenta: [chalk.white.bgMagenta.bold, chalk.white.bgMagenta.bold],
+    white: [chalk.whiteBright, chalk.whiteBright],
+    bgWhite: [chalk.black.bgWhiteBright, chalk.black.bgWhiteBright],
+};
 
-const logFormatConsole = printf(({ level, message, timestamp: ts, stack, [Symbol.for('splat')]: sp }) => {
-    // console.log(`logFormatConsole Called, level: ${level}, message: ${message}`);
-    const { callerHierarchy, uniqueId, textColor, loggingPrefix, lineSep } =
+const levelToChalkColor = {
+    unhandledexception: [chalk.white.bgRgb(255, 0, 0).bold, chalk.white.bgRgb(255, 0, 0).bold],
+    unreachable: [chalk.white.bgRgb(255, 0, 0).bold, chalk.white.bgRgb(255, 0, 0).bold],
+    catcherror: [chalk.bgRgb(248, 100, 90).whiteBright, chalk.bgRgb(248, 100, 90).whiteBright],
+    severe: [chalk.white.bgRgb(163, 0, 10).bold, chalk.white.bgRgb(163, 0, 10).bold],
+    error: [chalk.white.bgRed.bold, chalk.white.bgRed.bold],
+    hiccup: [chalk.red.bold, chalk.red.bold],
+    warning: [chalk.white.bgYellow.bold, chalk.white.bgYellow.bold],
+    info: [chalk.cyan, chalk.cyan],
+    verbose: [chalk.white.bgGreenBright.bold, chalk.white.bgGreenBright.bold],
+    debug: [chalk.white.bgMagentaBright.bold, chalk.white.bgMagentaBright.bold],
+    trace: [chalk.bgRgb(242, 140, 40).bold, chalk.bgRgb(242, 140, 40).bold],
+    billy: [chalk.black.bgWhiteBright, chalk.black.bgWhiteBright],
+};
+
+/**
+ * formatMultiLineStringToFixedLength(str, widthOfWindow)
+ * Formats multi line string every line to fixed width of the window
+ * so the background color doesnt look uneven on multiple lines, but
+ * it would look like a rectangle full of background color.
+ */
+function formatMultiLineStringToFixedLength(str, isErrorStack, formatLastLine = true, widthOfWindow = 120) {
+    str = str.split('\n');
+    str = str.map((line, index, arr) => {
+        if (index === arr.length - 1 && formatLastLine) {
+            return line;
+        }
+        if (index === 0 && !isErrorStack) {
+            let posOnTerminal = getColPosOnTerminal();
+            let tabIndex = 0;
+            while (line.charAt(tabIndex) === '\t') {
+                tabIndex++;
+            }
+            posOnTerminal += tabIndex * 7;
+            return line.padEnd(Math.ceil((posOnTerminal - 1 + line.length) / widthOfWindow) * widthOfWindow - posOnTerminal + 1, ' ');
+        }
+        return line.padEnd(Math.ceil(line.length / widthOfWindow) * widthOfWindow, ' ');
+    });
+    str = str.join('\n');
+    return str;
+}
+
+/**
+ *
+ *
+ *
+ *
+ */
+const lastWriteLineSepObj = {};
+// eslint-disable-next-line no-unused-vars
+function logFormat(typeOfLogFormat, detailsObj, logFilename) {
+    lastWriteLineSepObj[logFilename] = Object.prototype.hasOwnProperty.call(lastWriteLineSepObj, logFilename)
+        ? lastWriteLineSepObj[logFilename]
+        : true;
+    let { level, message, stack } = detailsObj;
+    const { timestamp: ts, [Symbol.for('splat')]: sp } = detailsObj;
+
+    level = level === 'warn' ? 'warning' : level;
+    // console.log(`logFormat${typeOfLogFormat} Called, level: ${level}, message: ${message}`);
+    // eslint-disable-next-line prefer-const
+    let { callerHierarchy, uniqueId, textColor, loggingPrefix, lineSeparator } =
         sp !== undefined
             ? sp.slice(-1)[0]
-            : { callerHierarchy: '', uniqueId: '', textColor: undefined, loggingPrefix: LoggingPrefix.true, lineSep: LineSeparator.true };
+            : { callerHierarchy: '', uniqueId: '', textColor: undefined, loggingPrefix: LoggingPrefix.true, lineSeparator: LineSeparator.true };
+    callerHierarchy = callerHierarchy === undefined ? '' : callerHierarchy;
+    uniqueId = uniqueId === undefined ? '' : uniqueId;
+    loggingPrefix = loggingPrefix === undefined ? LoggingPrefix.true : loggingPrefix;
+    lineSeparator = lineSeparator === undefined ? LineSeparator.true : lineSeparator;
+
     let logMesg = [];
     if (loggingPrefix.name === true) {
-        ts !== undefined ? logMesg.push(ts) : null;
-        if (level === 'unreachable' || level === 'catcherror' || level === 'severe') {
-            uniqueId !== undefined ? logMesg.push(`[${uniqueId}]`) : null;
+        if (typeOfLogFormat === 'console') {
+            if (
+                uniqueId !== undefined &&
+                (level === 'unhandledexception' || level === 'unreachable' || level === 'catcherror' || level === 'severe')
+            ) {
+                logMesg.push(`[${uniqueId}]`);
+            }
+            if (level !== 'hiccup' && level !== 'info') {
+                logMesg.push(`${level.toUpperCase()}:`);
+            }
+        } else if (typeOfLogFormat === 'file') {
+            ts !== undefined ? logMesg.push(ts) : null;
+            uniqueId !== undefined ? logMesg.push(`[${uniqueId.padStart(9, ' ')}]`) : null;
+            logMesg.push(`[${padStartAndEnd(`${level === 'warn' ? 'WARNING' : level.toUpperCase()}`, 21, ' ')}]`);
         }
     }
     // If custom message is sent then, the custom message is merged with the first line of error message.
@@ -89,125 +131,52 @@ const logFormatConsole = printf(({ level, message, timestamp: ts, stack, [Symbol
         }
         stack = stack.join('\n');
     }
-    let levelToPrint = '';
-    if (loggingPrefix.name === true) {
-        if (level === 'warn') {
-            levelToPrint = 'warning';
-        } else if (level === 'hiccup' || level === 'info') {
-            levelToPrint = '';
-        } else {
-            levelToPrint = level;
-        }
-    }
-    if (levelToPrint !== '') {
-        logMesg.push(`${levelToPrint}:`.toUpperCase());
-    }
     logMesg.push(message);
-    if (
-        (level === 'unreachable' || level === 'catcherror' || level === 'severe') &&
-        callerHierarchy !== '' &&
-        sp !== undefined &&
-        lineSep.name === true
-    ) {
-        logMesg.push(`(${callerHierarchy})`);
+    if (lineSeparator.name === true && callerHierarchy !== '') {
+        if (
+            typeOfLogFormat === 'file' ||
+            (typeOfLogFormat === 'console' && (level === 'unreachable' || level === 'catcherror' || level === 'severe'))
+        ) {
+            logMesg.push(`(${callerHierarchy})`);
+        }
     }
     logMesg = logMesg.join(' ');
-    logMesg = logMesg.split('\n');
-    logMesg = logMesg.map((line, index, arr) => {
-        if (index === arr.length - 1 && lineSep.name === false && stack === undefined) {
-            return line;
-        }
-        if (index === 0) {
-            let posOnTerminal = getColPosOnTerminal();
-            let tabIndex = 0;
-            while (line.charAt(tabIndex) === '\t') {
-                tabIndex++;
-            }
-            posOnTerminal += tabIndex * 7;
-            return line.padEnd(Math.ceil((posOnTerminal - 1 + line.length) / 120) * 120 - posOnTerminal + 1, ' ');
-        }
-        return line.padEnd(Math.ceil(line.length / 120) * 120, ' ');
-    });
-    logMesg = logMesg.join('\n');
-    if (lineSep.name) {
-        logMesg = `${logMesg}\n`;
+    logMesg = [logMesg];
+    if (typeOfLogFormat === 'console') {
+        logMesg[0] = formatMultiLineStringToFixedLength(logMesg[0], false, lineSeparator.name === false && stack === undefined);
     }
     if (stack !== undefined) {
-        if (!lineSep.name) {
-            logMesg = `${logMesg}\n`;
-        }
-        stack = stack.split('\n');
-        stack = stack.map((line, index, arr) => {
-            if (index === arr.length - 1 && lineSep.name === false) {
-                return line;
-            }
-            return line.padEnd(Math.ceil(line.length / 120) * 120, ' ');
-        });
-        stack = stack.join('\n');
-        if (lineSep.name) {
-            stack = `${stack}\n`;
-        }
+        logMesg.push(formatMultiLineStringToFixedLength(stack, true, lineSeparator.name === false));
     }
-    if (textColor !== undefined) {
-        if (textColor.name === 'cyan') {
-            logMesg = chalk.cyan(logMesg);
-        } else if (textColor.name === 'bgCyan') {
-            logMesg = chalk.bgCyan.bold(logMesg);
-        } else if (textColor.name === 'cyanBold') {
-            logMesg = chalk.cyan.bold(logMesg);
-        } else if (textColor.name === 'green') {
-            logMesg = chalk.green.bold(logMesg);
-        } else if (textColor.name === 'bgGreen') {
-            logMesg = chalk.white.bgGreen.bold(logMesg);
-        } else if (textColor.name === 'yellow') {
-            logMesg = chalk.yellow.bold(logMesg);
-        } else if (textColor.name === 'bgYellow') {
-            logMesg = chalk.white.bgYellow.bold(logMesg);
-        } else if (textColor.name === 'red') {
-            logMesg = chalk.red.bold(logMesg);
-        } else if (textColor.name === 'bgRed') {
-            logMesg = chalk.white.bgRed.bold(logMesg);
-        } else if (textColor.name === 'magenta') {
-            logMesg = chalk.magenta.bold(logMesg);
-        } else if (textColor.name === 'bgMagenta') {
-            logMesg = chalk.white.bgMagenta.bold(logMesg);
-        } else if (textColor.name === 'white') {
-            logMesg = chalk.whiteBright(logMesg);
-        } else if (textColor.name === 'bgWhite') {
-            logMesg = chalk.black.bgWhiteBright(logMesg);
+    if (typeOfLogFormat === 'console') {
+        if (textColor !== undefined && Object.prototype.hasOwnProperty.call(stringToChalkColor, textColor)) {
+            logMesg[0] = stringToChalkColor[textColor.name][0](logMesg[0]);
+            logMesg[1] !== undefined ? (logMesg[1] = stringToChalkColor[textColor.name][1](logMesg[1])) : null;
+        } else if (Object.prototype.hasOwnProperty.call(levelToChalkColor, level)) {
+            logMesg[0] = levelToChalkColor[level][0](logMesg[0]);
+            logMesg[1] !== undefined ? (logMesg[1] = levelToChalkColor[level][1](logMesg[1])) : null;
+        } else {
+            logMesg[0] = chalk.inverse(logMesg);
+            logMesg[1] !== undefined ? (logMesg[1] = chalk.inverse(logMesg)) : null;
         }
     }
-    // TODO: create stack color similar to the main color as for catcherror, if a custom color is given, as in the situation above, create similar to the custom color
-    if (level === 'unhandledexception') {
-        textColor === undefined ? (logMesg = chalk.white.bgRgb(255, 0, 0).bold(logMesg)) : null;
-    } else if (level === 'unreachable') {
-        textColor === undefined ? (logMesg = chalk.white.bgRgb(255, 0, 0).bold(logMesg)) : null;
-    } else if (level === 'catcherror') {
-        textColor === undefined ? (logMesg = chalk.bgRgb(248, 100, 90).whiteBright(logMesg)) : null;
-    } else if (level === 'severe') {
-        textColor === undefined ? (logMesg = chalk.white.bgRgb(163, 0, 10).bold(logMesg)) : null;
-    } else if (level === 'error') {
-        textColor === undefined ? (logMesg = chalk.white.bgRed.bold(logMesg)) : null;
-    } else if (level === 'hiccup') {
-        textColor === undefined ? (logMesg = chalk.red.bold(logMesg)) : null;
-    } else if (level === 'warn') {
-        textColor === undefined ? (logMesg = chalk.white.bgYellow.bold(logMesg)) : null;
-    } else if (level === 'info') {
-        textColor === undefined ? (logMesg = chalk.cyan(logMesg)) : null;
-    } else if (level === 'verbose') {
-        textColor === undefined ? (logMesg = chalk.white.bgGreenBright.bold(logMesg)) : null;
-    } else if (level === 'debug') {
-        textColor === undefined ? (logMesg = chalk.white.bgMagentaBright.bold(logMesg)) : null;
-    } else if (level === 'trace') {
-        textColor === undefined ? (logMesg = chalk.bgRgb(242, 140, 40).bold(logMesg)) : null;
-    } else if (level === 'billy') {
-        textColor === undefined ? (logMesg = chalk.black.bgWhiteBright(logMesg)) : null;
-    } else {
-        textColor === undefined ? (logMesg = chalk.inverse(logMesg)) : null;
+    logMesg = logMesg.join('\n');
+    if (typeOfLogFormat === 'file') {
+        if (loggingPrefix.name === true && !lastWriteLineSepObj[logFilename]) {
+            logMesg = `\n${logMesg}`;
+        }
     }
-    logMesg += stack !== undefined ? `${chalk.bgRgb(248, 131, 121).whiteBright(stack)}` : '';
+    lastWriteLineSepObj[logFilename] = lineSeparator.name;
+    lineSeparator.name ? (logMesg = `${logMesg}\n`) : null;
     return logMesg;
-});
+}
+
+// Define log functions
+/* #region logFormatFile and logFormatConsole : Begin */
+const logFormatFile = (logFilename) => printf((detailsObj) => logFormat('file', detailsObj, logFilename));
+
+const logFormatConsole = printf((detailsObj) => logFormat('console', detailsObj, undefined));
+
 /* #endregion logFormatFile and logFormatConsole : End */
 
 export { logFormatFile, logFormatConsole };
