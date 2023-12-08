3caf02b177e873b47aee2ed0dd1a8d1dcce98934
Anand Phulwani <anand_phulwani@yahoo.com>
2023-12-08 15:39:32 +0530

    Added new `doAllotment` function.

--- functions/allotment.js
+++ functions/allotment.js
@@ -124,5 +124,118 @@ function getManualAllotmentContractorIndex(usernameAndDealerFolderName, imageCou
     }
     return contractorsIndex;
 }
+
+/**
+ * Once the minimum DealerFolders for each contractors is alloted, using the pre allotted quantity
+ * as the initial alloted quantity for the contractors, continuing there on
+ * alloting to contractors as per config as in to maintain ratios, devised from the
+ * quantity(ImagesQty) parameters in the config, to generate `Example03` below,
+ * and updating the columns
+ */
+/* #region: Examples */
+/**
+ * `Example03`
+ * [
+ * // [ 'NameOfContractor', NormalThreshold, RatioOfThreshHoldWithOtherContractors, ImagesAlloted, RatioOfImagesAlloted, AllotmentPriority(RatioOfThreshHoldWithOtherContractors - RatioOfImagesAlloted) ],
+ *    [ 'ram', 300, 43, 50, 26, 17 ],
+ *    [ 'karan', 100, 14, 40, 21, -7 ],
+ *    [ 'pavan', 100, 14, 40, 21, -7 ],
+ *    [ 'arjun', 100, 14, 30, 16, -2 ],
+ *    [ 'om', 100, 14, 30, 16, -2 ]
+ * ]
+ */
+/* #endregion */
+async function doAllotment(
+    dealerDirectories,
+    contractors,
+    lotIndex,
+
+    isManualAllotment = false,
+    debug = false
+) {
+    let doesDestinationFolderAlreadyExists = false;
+    const { lotCfgMinDealerFolders, lotCfgImagesQty } = getLotConfigPropertiesValues(lotIndex);
+    const isDryRun = dealerDirectories[0].uniqueId === null;
+    if (isDryRun) {
+        /** Running a dry run */
+        /* #region validation checks */
+        if (lotCfgImagesQty !== undefined) {
+            let imagesQtyAllotedInCurrentLot = 0;
+            for (let index = 0; index < dealerDirectories.length; index++) {
+                if (lotCfgMinDealerFolders !== undefined && index < lotCfgMinDealerFolders) {
+                    // eslint-disable-next-line no-continue
+                    continue;
+                }
+                if (imagesQtyAllotedInCurrentLot > lotCfgImagesQty) {
+                    lgu(
+                        `Unable to continue the allotment because the condition is true: imagesQtyAllotedInCurrentLot(${imagesQtyAllotedInCurrentLot}) > lotCfgImagesQty(${lotCfgImagesQty})`
+                    );
+                    process.exit(1);
+                }
+                imagesQtyAllotedInCurrentLot += dealerDirectories[index].imageCount;
+            }
+        }
+        /* #endregion */
+        for (let index = 0; index < dealerDirectories.length; index++) {
+            const currentDealerDirectoryObj = getDealerDirectoryObjWithMaxImageCount(dealerDirectories);
+            let contractorsIndex;
+
+            if (isManualAllotment) {
+                contractors = recalculateAllotmentPriority(recalculateRatioOfImagesAlloted(contractors));
+                const { usernameAndDealerFolderName, imageCount } = currentDealerDirectoryObj;
+                contractorsIndex = getManualAllotmentContractorIndex(usernameAndDealerFolderName, imageCount, contractors);
+            } else if (lotCfgMinDealerFolders !== undefined && index < lotCfgMinDealerFolders) {
+                /* allotment by minimum dealerFolders for each contractors as per config */
+                contractorsIndex = index % contractors.length;
+            } else if (lotCfgImagesQty !== undefined && lotCfgImagesQty > 0) {
+                /* allotment by imagesQty */
+                contractors = recalculateAllotmentPriority(recalculateRatioOfImagesAlloted(contractors));
+                contractorsIndex = getIndexOfHighestIn2DArrayColumn(contractors, 5);
+            } else {
+                /* #region unreachable error mesg creation */
+                const mesg = [];
+                mesg.push(`Unable to allot the dealerDirectory(${currentDealerDirectoryObj.dealerFolderPath}) at this index(${index}), because:`);
+                mesg.push(`    a. It is an automatic allotment.`);
+                mesg.push(`    b. It is not in 'allotment by minimumDealerFolders for each contractors', because the below condition is false`);
+                mesg.push(
+                    `lotCfgMinDealerFolders(${lotCfgMinDealerFolders}) !== undefined && index(${index}) < lotCfgMinDealerFolders(${lotCfgMinDealerFolders})`
+                );
+                mesg.push(`    c. It is not in 'allotment by imagesQty', because the below condition is false`);
+                mesg.push(`lotCfgImagesQty(${lotCfgImagesQty}) !== undefined && lotCfgImagesQty(${lotCfgImagesQty}) > 0`);
+                /* #endregion */
+                lgu(mesg.join('\n'));
+                process.exit(1);
+            }
+            const [contractorAlloted] = contractors[contractorsIndex];
+            currentDealerDirectoryObj.contractorAlloted = contractorAlloted;
+            /* Adding allotment of images to the currentAllotment for all contractors */
+            contractors[contractorsIndex][3] += currentDealerDirectoryObj.imageCount;
+        }
+
+        for (let index = 0; index < dealerDirectories.length; index++) {
+            dealerDirectories[index].uniqueId = zeroPad(lotIndex, 2) + zeroPad(index + 1, 3);
+            const allotmentMesg = getAllotmentMesgForFolder(dealerDirectories[index], true);
+            !isManualAllotment ? lgi(allotmentMesg, Color.bgCyan) : null;
+            /* #region Check paths already exists */
+            const pathsToCheck = [dealerDirectories[index].destinationRecordKeepingPath, dealerDirectories[index].destinationPath];
+            for (let i = 0; i < pathsToCheck.length; i++) {
+                if (fs.existsSync(pathsToCheck[i])) {
+                    lge(`Folder: ${pathsToCheck[i]} already exists, cannot process ${dealerDirectories[index].dealerFolderPath} to its location.`);
+                    doesDestinationFolderAlreadyExists = true;
+                }
+            }
+            /* #endregion */
+        }
+    } else {
+        /** Running actualy allotment, confirmed in the previous dryRun */
+        for (let index = 0; index < dealerDirectories.length; index++) {
+            await executeSingleFolderAllotment(dealerDirectories[index]);
+            const allotmentMesg = getAllotmentMesgForFolder(dealerDirectories[index], false);
+            lgi(allotmentMesg, Color.bgGreen);
+        }
+    }
+    return doesDestinationFolderAlreadyExists;
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export { doAllotment };
