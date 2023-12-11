99cd99d2aaa4047ad3719155e253cccc912c0d99
Anand Phulwani <anand_phulwani@yahoo.com>
2023-12-11 13:33:14 +0530

    Added `keyInYNWithTimeout.js` functionality.

---
+++ functions/keyInYNWithTimeout.js
@@ -0,0 +1,33 @@
+import readline from 'readline';
+
+export default function keyInYNWithTimeout(question, timeout = 5000, defaultOption = false) {
+    return new Promise((resolve) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        const askQuestion = () => {
+            rl.question(`${question} (Y/N) `, (answer) => {
+                const upperCaseAnswer = answer.toUpperCase();
+                if (upperCaseAnswer === 'Y' || upperCaseAnswer === 'N') {
+                    rl.close();
+                    clearTimeout(timeoutId);
+                    resolve(upperCaseAnswer === 'Y');
+                } else {
+                    console.log('Invalid input. Please enter "Y" or "N".');
+                    askQuestion(); // Ask again
+                }
+            });
+        };
+
+        const timeoutId = setTimeout(() => {
+            rl.close();
+            console.log('\n');
+            // console.log(`\nTimeout - Default option '${defaultOption ? 'Yes' : 'No'}' selected`);
+            resolve(defaultOption); // Resolve with the default option
+        }, timeout);
+
+        askQuestion();
+    });
+}
