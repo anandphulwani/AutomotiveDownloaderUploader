ce681ce765386144fc31e39343d1666dec7280b2
Anand Phulwani <anand_phulwani@yahoo.com>
2023-12-19 17:54:54 +0530

    Improved `keyInYNWithTimeout` fn.

--- functions/keyInYNWithTimeout.js
+++ functions/keyInYNWithTimeout.js
@@ -1,36 +1,45 @@
 import readline from 'readline';
 
 /* eslint-disable import/extensions */
-import { printSectionSeperator } from './others.js';
+import { clearLastLinesOnConsole } from './consolesupportive.js';
 /* eslint-enable import/extensions */
 
 export default function keyInYNWithTimeout(question, timeout = 5000, defaultOption = false) {
     return new Promise((resolve) => {
+        let timeoutId;
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout,
         });
 
-        const askQuestion = () => {
-            rl.question(`${question} (Y/N) `, (answer) => {
+        const askQuestion = (isWrongResponse) => {
+            resetTimeout();
+            rl.question(`${isWrongResponse ? 'Invalid input, ' : ''}${question} (Y/N):`, (answer) => {
+                resetTimeout();
                 const upperCaseAnswer = answer.toUpperCase();
                 if (upperCaseAnswer === 'Y' || upperCaseAnswer === 'N') {
-                    rl.close();
                     clearTimeout(timeoutId);
-                    resolve(upperCaseAnswer === 'Y');
+                    rl.close();
+                    resolve({ answer: upperCaseAnswer === 'Y', isDefaultOption: false });
                 } else {
-                    console.log('Invalid input. Please enter "Y" or "N".');
-                    askQuestion(); // Ask again
+                    clearLastLinesOnConsole(1);
+                    askQuestion(true); // Ask again
                 }
             });
         };
 
-        const timeoutId = setTimeout(() => {
-            rl.close();
-            console.log('\r');
-            resolve(defaultOption); // Resolve with the default option
-        }, timeout);
+        const resetTimeout = () => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                console.log('\r');
+                process.stdout.moveCursor(0, -1);
+                process.stdout.clearLine();
+                console.log('Timed out.');
+                rl.close();
+                resolve({ answer: defaultOption, isDefaultOption: true }); // Resolve with the default option
+            }, timeout);
+        };
 
-        askQuestion();
+        askQuestion(false);
     });
 }
