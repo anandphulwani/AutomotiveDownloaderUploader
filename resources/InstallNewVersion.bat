666f18025c7868aec4d960972ceba7cad46157a3
Anand Phulwani <anand_phulwani@yahoo.com>
2024-01-05 17:19:24 +0530

    No need to copy `Reports` from the older version.

--- resources/InstallNewVersion.bat
+++ resources/InstallNewVersion.bat
@@ -57,7 +57,6 @@ rem Copy required files from last directory
 copy /Y "..\%lastDir%\configs\config.js" ".\configs\"
 copy /Y "..\%lastDir%\configs\config-user.js" ".\configs\"
 xcopy /E /I /Y "..\%lastDir%\configs\DealerConfiguration\*.*" ".\configs\DealerConfiguration"
-xcopy /E /I /Y "..\%lastDir%\datastore\Reports\*.*" ".\datastore\Reports"
 
 echo ewogICAicm9vdHMiOiB7CiAgICAgICJib29rbWFya19iYXIiOiB7CiAgICAgICAgICJjaGlsZHJlbiI6IFtdLAogICAgICAgICAiZGF0ZV9hZGRlZCI6ICIxMzMyMTA5NzE4MzQ2NTgzNCIsCiAgICAgICAgICJkYXRlX2xhc3RfdXNlZCI6ICIwIiwKICAgICAgICAgImRhdGVfbW9kaWZpZWQiOiAiMTMzMjg5NjA4Mjc3Nzc1NTEiLAogICAgICAgICAiZ3VpZCI6ICIwYmM1ZDEzZi0yY2JhLTVkNzQtOTUxZi0zZjIzM2ZlNmM5MDgiLAogICAgICAgICAiaWQiOiAiMSIsCiAgICAgICAgICJuYW1lIjogIkJvb2ttYXJrcyBiYXIiLAogICAgICAgICAidHlwZSI6ICJmb2xkZXIiCiAgICAgIH0KICAgfSwKICAgInZlcnNpb24iOiAxCn0= > %TEMP%\temp.b64 && certutil -decode -f %TEMP%\temp.b64 ..\autodown_datastore\Bookmarks && del %TEMP%\temp.b64
 xcopy /E /I /Y "..\autodown_datastore\Bookmarks" "..\autodown_datastore\Bookmarks_Blank"
