fba9f2db0542ad0c4f2282b9ad913150f127599b
Anand Phulwani <anand_phulwani@yahoo.com>
2024-01-23 18:19:02 +0530

    Updated `encryptdecrypt.js` to creater shorter encryption string.

--- functions/encryptdecrypt.js
+++ functions/encryptdecrypt.js
@@ -1,25 +1,21 @@
 import crypto from 'crypto';
 
+const FIXED_IV = Buffer.from([0x40, 0x71, 0x13, 0x41, 0x2c, 0x82, 0x88, 0x1f, 0x9c, 0xff, 0x28, 0xa4, 0x0b, 0xda, 0x05, 0x95]);
+
 // Function to encrypt text
 function encrypt(text, salt) {
-    const secretKey = crypto.createHash('sha256').update(salt).digest();
-    const iv = crypto.randomBytes(16); // Generate a random IV (initialization vector)
-    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+    const secretKey = crypto.createHash('md5').update(salt).digest();
+    const cipher = crypto.createCipheriv('aes-128-cbc', secretKey, FIXED_IV);
+    const encrypted = cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
+    return encrypted;
 }
 
 // Function to decrypt text
 function decrypt(text, salt) {
-    const secretKey = crypto.createHash('sha256').update(salt).digest();
-    const textParts = text.split(':');
-    const iv = Buffer.from(textParts.shift(), 'hex');
-    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
+    const secretKey = crypto.createHash('md5').update(salt).digest();
+    const decipher = crypto.createDecipheriv('aes-128-cbc', secretKey, FIXED_IV);
+    const decrypted = decipher.update(text, 'hex', 'utf8') + decipher.final('utf8');
+    return decrypted;
 }
 
 export { encrypt, decrypt };
