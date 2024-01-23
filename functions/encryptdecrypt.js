a7c84717e4bffed0d2958cd6b9d800f4792feed1
Anand Phulwani <anand_phulwani@yahoo.com>
2024-01-23 15:55:35 +0530

    Added `encrypt` and `decrypt` functionality.

---
+++ functions/encryptdecrypt.js
@@ -0,0 +1,23 @@
+import crypto from 'crypto';
+
+// Function to encrypt text
+function encrypt(text, secretKey) {
+    const iv = crypto.randomBytes(16); // Generate a random IV (initialization vector)
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+}
+
+// Function to decrypt text
+function decrypt(text, secretKey) {
+    const textParts = text.split(':');
+    const iv = Buffer.from(textParts.shift(), 'hex');
+    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+export { encrypt, decrypt };
